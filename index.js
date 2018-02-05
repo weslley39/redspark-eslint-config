af9090a5ccb3ebc9d5e83de41fb13b9a28174be2
Weslley Neri <weslley39@gmail.com>
2018-02-05 16:47:00 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": "airbnb",
-  "plugins": [ ],
+  "extends": ["airbnb", "airbnb-base/legacy"],
+  "plugins": ["react", "angular"],
   "rules": {
     "react/no-array-index-key": 0,
     "react/prefer-stateless-function": 1,
