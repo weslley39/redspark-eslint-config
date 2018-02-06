9b169600cad9d3f801e2474da3aec9efb788fa02
Weslley Neri <weslley39@gmail.com>
2018-02-06 15:47:25 -0200

    removed react rules for base js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ module.exports = {
   "extends": ["airbnb-base/legacy"],
   "plugins": [],
   "rules": {
-    "react/no-array-index-key": 0,
-    "react/prefer-stateless-function": 1,
     "import/no-unresolved": [2, { ignore: ['meteor/*'] }],
     "import/no-extraneous-dependencies": 0,
     "import/no-absolute-path": 0,
@@ -14,19 +12,12 @@ module.exports = {
     "one-var": 0,
     "consistent-return": 0,
     "no-empty": 0,
-    "react/wrap-multilines": 0,
-    "jsx-a11y/img-has-alt": 0,
     "global-require": 0,
     "no-param-reassign": 1,
     "import/extensions": 0,
     "no-case-declarations": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/require-default-props": 0,
-    "react/sort-comp": 0,
     "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "react/forbid-prop-types": 0,
-    "react/prefer-stateless-function": 0
+    "import/extensions": 0
   },
   "parserOptions":{
     "ecmaFeatures": {
